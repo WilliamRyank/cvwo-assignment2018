270266142f4aa603b45281308ab265a2d83fc2a0
williamrk <kwilliamryan@yahoo.com>
2018-12-27 16:47:10 +0700

    adding CRUD routes for tasks, making layouts for home page and form to create new task, generate table for Task model

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,16 @@
+class TasksController < ApplicationController
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:task, :description)
+    end
+end
