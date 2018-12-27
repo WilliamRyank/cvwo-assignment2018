f68a510d121455dd4b7388004af1e28d18d8b96b
williamrk <kwilliamryan@yahoo.com>
2018-12-27 17:44:13 +0700

    finishing with CRUD routes and layouts for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,49 @@
 class TasksController < ApplicationController
 
+  def index
+    @tasks = Task.all
+  end
+
   def new
     @task = Task.new
   end
 
   def create
     @task = Task.new(task_params)
-    @task.save
+    if @task.save
+      flash[:notice] = "Successfully added new task"
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:notice] = "Successfully edited new task"
+      redirect_to task_path(@task)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    if @task.destroy
+      flash[:notice] = "Task was successfully destroyed"
+      redirect_to tasks_path
+    else
+      render "tasks"
+    end
   end
 
   private
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,3 @@
 class Task < ApplicationRecord
-
+  validates :task, presence: true, length: {minimum:3, maximum: 25}
 end
