248adf68c333677e48f2b2ad57d6140dd2b4f50b
williamrk <kwilliamryan@yahoo.com>
2019-01-04 13:40:55 +0700

    add secure password to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_04_062115) do
+ActiveRecord::Schema.define(version: 2019_01_04_063351) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "task_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_01_04_062115) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
   end
 
 end
